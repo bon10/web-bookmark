177f58e256a730aff1eb59409b50c0fa5a6d8b1f
bon <bontyun@gmail.com>
2023-05-25 18:55:55 +0900

    キャッシュ機構の実装

--- app/next.config.js
+++ app/next.config.js
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [process.env.AWS_BUCKET_DOMAIN],
+    //domains: ['production-tube-bookmark-bucket.s3.ap-northeast-1.amazonaws.com'],
+  },
+  async headers () {
+    return [
+      {
+       source: '/(.*).(jpg|png)',
+       headers: [
+         {
+           key: 'Cache-Control',
+           value:
+             'public, max-age=300, s-maxage=300', // 5 minutes
+         },
+       ],
+      }
+    ]
+  },
 }
 
 module.exports = nextConfig
